5aadd8aebdf584ce3d379a01ccef3041b573e982
zimbear3103 <kiendhduc@gmail.com>
2023-10-31 01:53:43 +0700

    add flow UI for StudioPage

---
+++ Presentaion/Pages/SchedulePage/ScheduleUpdate.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Presentaion.Pages.SchedulePage
+{
+    public class ScheduleUpdateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
