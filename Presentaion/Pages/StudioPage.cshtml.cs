0409050f499908baac8f3bedd6f35992d53d6894
LNPhuc <lnphuc2006@gmail.com>
2023-10-31 00:07:05 +0700

    merge main into phuc

---
+++ Presentaion/Pages/StudioPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Presentaion.Pages
+{
+    public class StudioPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
