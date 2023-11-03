43a51a1adfbfe4f03d3383ad772d0fafc8614cc8
Kim Ta <hoangkim.lol2002@gmail.com>
2023-11-03 11:16:41 +0700

    Customer Change

--- BusinessLogic/IService/ICustomerService.cs
+++ BusinessLogic/IService/ICustomerService.cs
@@ -1,6 +1,9 @@
-﻿namespace BusinessLogic.IService;
+﻿using DataAccess.DataAccess;
+
+namespace BusinessLogic.IService;
 
 public interface ICustomerService
 {
-    
+    Customer GetCusById(Guid id);
+   /* Customer Update(Guid id, Customer customer,Account account);*/
 }
--- DataAccess/IRepository/ICustomerRepository.cs
+++ DataAccess/IRepository/ICustomerRepository.cs
@@ -5,5 +5,10 @@ namespace DataAccess.IRepository;
 
 public interface ICustomerRepository : IGenericRepository<Customer>
 {
-    
+    Customer GetCusById(Guid id);
+    /*Customer Update(Customer customer);*/
+    void SaveChanges();
+    /*Boolean IsEmailExist(string email);
+    Boolean IsPhoneExist(string phone);
+    Boolean IsChange(Customer cusold, Customer cusnew);*/
 }
