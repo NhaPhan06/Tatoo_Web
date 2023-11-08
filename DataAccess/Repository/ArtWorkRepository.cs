e6bc79c1aca0b3bd735d63042456261a83d11247
Thong <thongpmse161425@fpt.edu.vn>
2023-11-08 11:21:29 +0700

    artwork Up

--- DataAccess/Repository/ArtWorkRepository.cs
+++ DataAccess/Repository/ArtWorkRepository.cs
@@ -6,7 +6,15 @@ namespace DataAccess.Repository;
 
 public class ArtWorkRepository: GenericRepository<ArtWork>, IArtWorkRepository
 {
+    private readonly TatooWebContext _context;
+    
     public ArtWorkRepository(TatooWebContext context) : base(context)
     {
+        _context=context;
+    }
+    public ArtWork Create(ArtWork ArtWork)
+    {
+        _context.Set<ArtWork>().Add(ArtWork);
+        return ArtWork;
     }
 }
